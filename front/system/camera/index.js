a10fd6643002d1ad894dcf1bc338df78986d96d8
Marvin SANT <contact@devteck.eu>
2017-03-09 22:44:44 +0100

    :art: Miniclean

--- front/system/camera/index.js
+++ front/system/camera/index.js
@@ -6,8 +6,7 @@ import 'tracking'
 
 module.exports = ( options = {} ) => {
 
-    var readyVR = false
-
+    var readyUX = false
     var tracked = []
 
     const VR = ({ doms }) => {
@@ -15,26 +14,6 @@ module.exports = ( options = {} ) => {
         const { canvasScreen, canvasVR, video } = doms
 
         const stats = new Stats()
-
-        // TRACKER §!!
-        tracking.ColorTracker.registerColor('b', function(r, g, b) {
-            var dx = r - 245;
-            var dy = g - 248;
-            var dz = b - 255;
-            if ((b - g) >= 100 && (r - g) >= 60) {
-              return true;
-            }
-            return dx * dx + dy * dy + dz * dz < 3500;
-        });
-
-        const tracker = new tracking.ColorTracker(['b']);
-
-        tracking.track(video, tracker, { camera: true });
-
-        tracker.on('track', event => {
-            tracked = event.data.filter( x => x.width > 400 && x.height > 300)
-        })
-        // TRACKER §!!
 
         stats.dom.style='display: flex; top:0;'
         document.querySelector('.display').appendChild( stats.dom )
@@ -44,19 +23,17 @@ module.exports = ( options = {} ) => {
         const ctxScreen = canvasScreen.getContext('2d')
         const ctxVr = canvasVR.getContext('2d')
 
-
-        const getData = ( x, y, width, height ) => {
+        /*
+        const takeScreenshot = ( x, y, width, height ) => {
             canvasScreen.width = width
             canvasScreen.height = height
             ctxScreen.drawImage(video, -x, -y );
             return canvasScreen.toDataURL('image/jpeg');
         }
+        */
 
         const animate = () => {
             stats.begin();
-
-            // VR
-
 
             tracked.forEach( elem => {
                 ctxVr.beginPath();
@@ -73,10 +50,6 @@ module.exports = ( options = {} ) => {
 
             // ctxVr.drawImage( video, 0, 0, video.clientWidth, video.clientHeight )
 
-
-
-
-
             stats.end();
             requestAnimationFrame( animate );
         }
@@ -88,11 +61,6 @@ module.exports = ( options = {} ) => {
             canvasVR.width = video.clientWidth
             canvasVR.height = video.clientHeight
 
-            const imgs = tracked.map( elem => {
-                return '<img src="'+getData( elem.x, elem.y, elem.width, elem.height )+'" />'
-            })
-
-            document.querySelector('.debug').innerHTML = imgs.join('')
         }, 1000)
 
     }
@@ -102,8 +70,8 @@ module.exports = ( options = {} ) => {
 
         store.watchAndCallImmediately( 'camera.dom', 'camera.device', 'camera.resolution', ( doms, device, resolution ) => {
 
-            if ( !readyVR ) {
-                readyVR = true
+            if ( !readyUX ) {
+                readyUX = true
                 VR({ doms })
             }
 
